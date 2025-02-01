6ec620452ad4aa7b6024cf2dd62dffa1bfd88655
Dip Patel <pateldeep2332@gmail.com>
2025-02-01 13:22:58 +0530

    created board view and implemented the lifecycle methods

--- Array-Minesweeper/header/Gameplay/Board/BoardController.h
+++ Array-Minesweeper/header/Gameplay/Board/BoardController.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../../header/Gameplay/Board/BoardView.h"
 
 namespace Gameplay {
 
@@ -20,6 +21,7 @@ namespace Gameplay {
 			void reset();
 
 		private:
+			BoardView* board_view;
 			void createBoard();
 			void destroy();
 			void deleteBoard();
--- Array-Minesweeper/source/Gameplay/Board/BoardController.cpp
+++ Array-Minesweeper/source/Gameplay/Board/BoardController.cpp
@@ -5,11 +5,11 @@ namespace Gameplay {
 	namespace Board {
 
 		BoardController::BoardController() {
-
+			board_view = new BoardView(this);
 		}
 
 		BoardController::~BoardController() {
-
+			destroy();
 		}
 
 		void BoardController::createBoard() {
@@ -17,15 +17,15 @@ namespace Gameplay {
 		}
 
 		void BoardController::initialize() {
-
+			board_view->initialize();
 		}
 
 		void BoardController::update() {
-
+			board_view->update();
 		}
 
 		void BoardController::render() {
-
+			board_view->render();
 		}
 
 		void BoardController::reset() {
@@ -37,7 +37,7 @@ namespace Gameplay {
 		}
 
 		void BoardController::destroy() {
-
+			delete(board_view);
 		}
 	}
 
