42a9976a970aee72f5d54600dd3b8c0025543aff
Dip Patel <pateldeep2332@gmail.com>
2025-02-06 11:28:44 +0530

    Implemented open neighbour empty cells

--- Array-Minesweeper/header/Gameplay/Board/BoardController.h
+++ Array-Minesweeper/header/Gameplay/Board/BoardController.h
@@ -37,10 +37,12 @@ namespace Gameplay
             int getMinesCount();
 
             void openCell(sf::Vector2i cell_position);
+            void openEmptyCells(sf::Vector2i cell_position);
             void openAllCells();
             void processCellInput(Cell::CellController* cell_controller, UI::UIElement::ButtonType button_type);
             void flagCell(sf::Vector2i cell_position);
             void processCellValue(sf::Vector2i cell_position);
+            void processEmptyCell(sf::Vector2i cell_position);
             void populateBoard(sf::Vector2i cell_position);
 
             int countMinesAround(sf::Vector2i cell_position);
